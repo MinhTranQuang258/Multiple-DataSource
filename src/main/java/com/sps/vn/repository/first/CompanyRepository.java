fbbd0394b9158a17d207331d6cba40c1cc890734
MinhTranQuang258 <finalleo258@gmail.com>
2018-06-16 02:39:30 +0700

    Update the implement

--- src/main/java/com/sps/vn/repository/first/CompanyRepository.java
+++ src/main/java/com/sps/vn/repository/first/CompanyRepository.java
@@ -1,12 +1,10 @@
 package com.sps.vn.repository.first;
 
-import javax.transaction.Transactional;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.sps.vn.entities.Company;
 
-@Transactional
 public interface CompanyRepository extends CrudRepository<Company, Long>{
 
 }
